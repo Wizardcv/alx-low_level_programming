1a1245b9724fe1928f17b1863afc3f7990286a14
Emmanuel <ayeniemmaose@gmail.com>
2022-09-08 08:39:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
  * It prints the "Programming is like building a multilingual puzzle
  * Returns 0 (success)
  */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
