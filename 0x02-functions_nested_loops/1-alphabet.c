6fdd614a6d18592fb0bb0cf0b5bbb3cb3b5421f6
Emmanuel <ayeniemmaose@gmail.com>
2022-09-13 03:57:31 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char b;
+for(b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
